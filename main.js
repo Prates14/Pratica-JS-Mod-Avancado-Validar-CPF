ec3af4072eb4167204c9212bcf68d10cb78c6205
Prates <guipratesdsantos@gmail.com>
2023-07-12 16:59:41 -0300

    valida cpf

--- main.js
+++ main.js
@@ -34,11 +34,13 @@ EnvioDoCpf.prototype.calcPorDuasEtapas = function (digito1) {
   //Primeira etapa do calculo
   let calculo1 = [...digito1]
   const res1 = calculo1.reduce((acumulador, valor, indice) => acumulador += (Number(valor) * (10 - indice)), 0)
-  const penultimoNum = (11 - (res1 % 11))
+  let penultimoNum = (11 - (res1 % 11))
+  if(penultimoNum > 9) penultimoNum = '0'
   //Segunda etapa do calculo
   let calculo2 = [...digito1, penultimoNum]
   const res2 = calculo2.reduce((acumulador, valor, indice) => acumulador += (Number(valor) * (11 - indice)), 0)
   const ultimoNum = (11 - (res2 % 11))
+  if(ultimoNum > 9) ultimoNum = '0'
   //Resultado final do calculo
   const res3 = [...calculo2, ultimoNum]
   const resultadoFinal = res3.reduce(function (ac, val) {
@@ -50,4 +52,4 @@ EnvioDoCpf.prototype.calcPorDuasEtapas = function (digito1) {
 
 //CPF dos usuários
 const usuario1 = new EnvioDoCpf('242.863.650-26')
-const usuario2 = new EnvioDoCpf('809.783.640-82')
+const usuario2 = new EnvioDoCpf('070.987.720-03')
